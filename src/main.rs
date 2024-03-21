29e03a51ce61f6b5b01f88688e60ad1184fe7744
DevJhojan <jhojan1806@gmail.com>
2024-03-20 22:53:23 -0500

    operations basics added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::result;
+
 fn main() {
     println!("Hello, world!");
+    println!("Result {}", result(2,4,"+")); 
+}
+
+fn result(number_one:i32, number_two:i32, operation:&str)->i32{
+    match operation {
+       "+" => number_one + number_two, 
+       "-" => number_one - number_two, 
+       "x" => number_one * number_two, 
+       "/" => number_one / number_two, 
+       "%" => number_one % number_two,
+       _=> {
+            println!("Invalid operation");
+            return -00000;
+       }
+    } 
 }
